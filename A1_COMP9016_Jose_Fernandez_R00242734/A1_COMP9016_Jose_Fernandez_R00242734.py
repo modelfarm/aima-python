095986b3af2b6b8a9de8ae04f2865091c34eaadd
potasio <joseluisfdez@gmail.com>
2023-10-08 01:04:37 +0100

    08oCT2023

--- A1_COMP9016_Jose_Fernandez_R00242734/A1_COMP9016_Jose_Fernandez_R00242734.py
+++ A1_COMP9016_Jose_Fernandez_R00242734/A1_COMP9016_Jose_Fernandez_R00242734.py
@@ -12,44 +12,56 @@ import random
 # AGENT CLASSES
 ########################################
 # Agent Class - Fire brigade Agent - Model
-def ModelFireman():
-    location = [0,0]
-    width = 6
-    height = 6
-    model = {(x, y): None for x in range(width) for y in range(height)}     
+class ModelFireman(Agent):
+    location = [0,0]           
+    Name = 'ModelFireman'
+    Agent.isHoldingWater = False
+    
+    def takeWater(self, thing):
+        '''returns True upon success or False otherwise'''
+        if isinstance(thing, Water):
+            return True
+        return False
+    
+    def estinguishFire(self, thing):
+        '''returns True upon success or False otherwise'''
+        if isinstance(thing, Fire):
+            return True
+        return False
 
-    def program(percept):
-        location, things = percept
-        print(model[tuple(location)])
-        # print(things)
-        
-        for t in things:
-            if model[tuple(location)] not in['Visited']:
-                if isinstance(t, Fire):
-                    print('there is a fire')
-                elif isinstance(t, Water):
-                    model[tuple(location)] = 'Water'
-                    print('there is a Water')
-                    return 'Extinguish'
-                model[tuple(location)] = t  # Update the model here
-        
-        if not isinstance(things,(Water,Fire)):
-            model[tuple(location)] = 'Visited'
-        
-                        
-        choice = random.choice(('L','R','D','U'))
-        if  choice !=0:
-            if choice == 'L':
-                pass
-            elif choice == 'R':
-                pass
-            elif choice == 'D':
-                pass
-            elif choice == 'U':
-                pass
-        return 'Move'+ choice
-         
-    return Agent(program)
+########################################
+# AGENT PROGRAM
+########################################
+def programAgent(percept):
+    location, things = percept
+    for t in things:
+        #if model[tuple(location)] not in['Visited']:
+        if isinstance(t, Fire):
+            model[tuple(location)] = 'Fire'
+            print('There is Fire')
+            return 'Estinguish Fire'
+        elif isinstance(t, Water):
+            model[tuple(location)] = 'Water'
+            print('There is Water')
+            return 'Take Water'     
+        model[tuple(location)] = t  # Update the model here
+
+    if not isinstance(model[tuple(location)],(Water,Fire)):
+        model[tuple(location)] = 'Visited'
+
+    choice = random.choice(('L','R','D','U'))
+    if  choice !=0:
+        if choice == 'L':
+            pass
+        elif choice == 'R':
+            pass
+        elif choice == 'D':
+            pass
+        elif choice == 'U':
+            pass
+    return 'Move' + choice
+
+    #return programAgent#, model
 
 ########################################
 # THING CLASSES
@@ -61,6 +73,7 @@ class Water(Thing):
 
 # Thing Class - Fire
 class Fire(Thing):
+    Name='Fire'
     pass
 
 
@@ -68,20 +81,7 @@ class Fire(Thing):
 # ENVIRONMENT CLASS
 ########################################
 # Forrest Class
-class Forrest(XYEnvironment):
-    # def percept(self, agent):
-    #     '''return a list of things that are in our agent's location'''
-    #     things = self.list_things_at(agent.location)
-    #     return things
-    
-    def __init__(self, width=5, height=5):
-        super().__init__(width, height)
-        model = {}
-        for x in range(width):
-            for y in range(height):
-                model[x,y] = None
-            
-            
+class Forrest2D(GraphicEnvironment):
     def percept(self, agent):
         listThings = []
         for things in self.list_things_at(agent.location):
@@ -94,65 +94,75 @@ class Forrest(XYEnvironment):
         return [Fire, Water, ModelFireman]
 
     def execute_action(self, agent, action):
-        #print('here there is a fire')
-        # try to optimize grabbing by using only the method can_grab from Agent Class
-        #agent.program('Extinguish')
         if action == 'MoveR':
             if agent.location[0]>=0 and agent.location[0]< 5:
                 agent.location[0]+=1
                 agent.performance -= 1
-                print('R here we go')       
+                #print('R here we go')       
         elif action == 'MoveL':
             if agent.location[0]>0 and agent.location[0]<=5:
                 agent.location[0]-=1
                 agent.performance -= 1
-                print('L here we go')         
+                #print('L here we go')         
         elif action == 'MoveU':
             if agent.location[1]>=0 and agent.location[1]<5:
                 agent.location[1]+=1
                 agent.performance -= 1
-                print('U here we go')               
+                #print('U here we go')               
         elif action == 'MoveD':
             if agent.location[1]>0 and agent.location[1]<=5:
                 agent.location[1]-=1
                 agent.performance -= 1             
-                print('D here we go')   
-        elif action == 'Extinguish':
-            #agent.location=(0,0)
-            self.delete_thing(agent)
-            if agent.location[0]>=0 and agent.location[0]< 5:
-                agent.location[0]+=1
-                agent.performance += 100
-            else:
-                agent.location[0]-=1
-                agent.performance += 100        
-            print('Extinguish here we go')
-        
-        print(agent.location)
+                #print('D here we go') 
+        elif action == 'Take Water':
+            items = self.list_things_at(agent.location, tclass=Water)
+            if len(items) != 0:
+                if agent.takeWater(items[0]): 
+                    agent.performance += 100
+                    self.delete_thing(items[0])
+                    agent.isHoldingWater = True
+                    print('Take Water')
+        elif action == 'Estinguish Fire':
+            items = self.list_things_at(agent.location, tclass=Fire)
+            if len(items) != 0:                  
+                if agent.estinguishFire(items[0]) and agent.isHoldingWater:
+                    agent.performance += 100
+                    self.delete_thing(items[0])
+                    agent.isHoldingWater = False
+                    print('Estinguish Fire')
+                else:
+                    agent.performance -= 100
+                    print('Burning, no water')
+                    
+        if model[tuple(agent.location)] == 'Visited':
+            agent.performance -= 10
             
+        print(model[tuple(agent.location)])
+        print(agent.location)
+        print(f'Fireman performance: {agent.performance} and is holding water: {agent.isHoldingWater}')
 
     def is_done(self):
         
         return False
+    
+collections
+collections.Iterable = collections.abc.Iterable
+collections.Sequence = collections.abc.Sequence   
+ 
+   
+model = {(x, y): None for x in range(6) for y in range(6)}
+forrest = Forrest2D(6,6, color={'ModelFireman': (230, 115, 40), 'Water': (0, 200, 200), 'Fire': (200,0,0)})
 
-
-
-# def RandomFiremanProgram(actions):
-#     return lambda percept: random.choice(actions)
-
-# firemanActions = ['Extenguish','Move']
-# percept = ['Fire', 'Tree', 'Axes']              
-
-# newFiremanProgram = RandomFiremanProgram(firemanActions)
-
-fireman = ModelFireman()
+fireman = ModelFireman(programAgent)
 fire = Fire()
 water = Water()
 
-forrest = Forrest()
-forrest.add_thing(fireman,[0,0])
-forrest.add_thing(water,(2,1))
+forrest.add_thing(fireman, [3,3])
+forrest.add_thing(water, [3,4])
+forrest.add_thing(fire, [3,5])   
 
-forrest.run()
+forrest.run(30,delay=0.1)
 
-print(f'Fireman performance: {fireman.performance}')
+print(f'Fireman perfrmance: {fireman.performance} and is holding water: {fireman.isHoldingWater}')
+
+print(model)
